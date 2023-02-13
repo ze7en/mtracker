9abf7668d0e927b29de18486a5e359c3d63f2fdb
Kim T <kimthorning@gmail.com>
2023-02-13 14:23:01 +0100

    refactor: adjusted heading if not logged in

--- src/components/screens/CheckIn.tsx
+++ src/components/screens/CheckIn.tsx
@@ -12,14 +12,10 @@ export const CheckIn = () => {
       <div className="hero">
         <div className="text-center hero-content">
           <div>
-            {state.state === 'SIGNED_IN' && <>
+            {state.state === 'SIGNED_IN' ? <>
               <h1 className="mb-5 text-5xl font-bold">Check in</h1>
               <CheckInForm state={ state } />
-            </>
-            }
-            {state.state !== 'SIGNED_IN' && <>
-              <h1 className="mb-5 text-5xl font-bold">Please sign in</h1>
-            </>
+            </> : <></>
             }
           </div>
         </div>
